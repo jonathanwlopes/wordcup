cea6863d9530341f97a02e6a5ac36e8192a3d0f9
Luan Lemos <luan.lemos@agenciaeplus.com.br>
2022-11-25 10:46:34 -0300

    style: Add component Footer

--- client/src/components/Footer/styles.ts
+++ client/src/components/Footer/styles.ts
@@ -3,6 +3,12 @@ import styled from 'styled-components'
 export const Footer = styled.footer`
   width: 100%;
   background-color: #3f4140;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 1rem 0;
+  position: absolute;
+  bottom: 0;
 `
 
 export const Logo = styled.img`
